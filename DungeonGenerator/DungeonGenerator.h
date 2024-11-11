2789418141a379cad5fb5b1e9aa7feb337b84aff
Kyle Peterson <kdpeters@gmail.com>
2024-11-11 10:38:27 -0500

    Initial Commit

---
+++ DungeonGenerator/DungeonGenerator.h
@@ -0,0 +1,18 @@
+//
+//  DungeonGenerator.h
+//  DungeonGenerator
+//
+//  Created by Kyle Peterson on 11/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DungeonGenerator.
+FOUNDATION_EXPORT double DungeonGeneratorVersionNumber;
+
+//! Project version string for DungeonGenerator.
+FOUNDATION_EXPORT const unsigned char DungeonGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DungeonGenerator/PublicHeader.h>
+
+
---
+++ DungeonGeneratorTests/DungeonGeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonGeneratorTests.swift
+//  DungeonGeneratorTests
+//
+//  Created by Kyle Peterson on 11/11/24.
+//
+
+import Testing
+@testable import DungeonGenerator
+
+struct DungeonGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
